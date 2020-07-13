f6e71f8ff481a2cc0af70e3ad4255cf7d96bd2db
jerryenuh <44992605+jerryenuh@users.noreply.github.com>
2020-07-13 11:43:42 -0500

    Created Employee Page

--- Webusitu/Employees.aspx
+++ Webusitu/Employees.aspx
@@ -1,15 +1,31 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="Webusitu.WebForm1" %>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
-</body>
-</html>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="Webusitu.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Image ID="Image1" runat="server" Height="117px" ImageUrl="~/Content/HEART-NSTA-Trust-Logo.png" Width="111px" />
+    <br />
+    <br />
+    <asp:Label ID="enteremplbl" runat="server" Font-Bold="True" Font-Size="20pt" Text="Enter Employees Below "></asp:Label>
+    <br />
+    <div>
+        ID:
+        <asp:TextBox ID="IDtxt" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Name :
+        <asp:TextBox ID="nametxt" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Email:
+        <asp:TextBox ID="emailtxt" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Telephone:
+        <asp:TextBox ID="telephonetxt" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Department:
+        <asp:TextBox ID="departmenttxt" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; JobTitle:
+        <asp:TextBox ID="jobtitletxt" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+        <br />
+    </div>
+</asp:Content>
--- Webusitu/Employees.aspx.cs
+++ Webusitu/Employees.aspx.cs
@@ -4,13 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Webusitu
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["LeaveApplicationSystemConnectionString"].ConnectionString);
+        SqlCommand cmd = new SqlCommand();
+        SqlParameter sp1 = new SqlParameter();
+        SqlParameter sp2 = new SqlParameter();
+        SqlParameter sp3 = new SqlParameter();
+        SqlParameter sp4 = new SqlParameter();
+        bool flag = false;
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //connection setup and opening
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["LeaveApplicationSystemConnectionString"].ConnectionString);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            //Code to send things to DB
+            long empid = Convert.ToInt32(IDtxt.Text);
+            long telephone = Convert.ToInt64(telephonetxt.Text);
+            cmd = new SqlCommand("spInsEmployee", connection);
+            cmd.Parameters.AddWithValue("ID", SqlDbType.Int).Value = empid;
+            cmd.Parameters.AddWithValue("@Name", SqlDbType.NChar).Value = nametxt.Text;
+            cmd.Parameters.AddWithValue("@Email", SqlDbType.NChar).Value = emailtxt.Text;
+            cmd.Parameters.AddWithValue("@Telephone", SqlDbType.Int).Value = telephone;
+            cmd.Parameters.AddWithValue("@Department", SqlDbType.NChar).Value = departmenttxt.Text;
+            cmd.Parameters.AddWithValue("@JobTitle", SqlDbType.NChar).Value = jobtitletxt.Text;
+            cmd.CommandType = CommandType.StoredProcedure;
+            //connection.Open();
+            cmd.ExecuteNonQuery();
+            //rd.Close();
+            connection.Close();
 
         }
     }
